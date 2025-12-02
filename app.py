813c350f74674ef32511741597f691ac6869932b
jessicabajamundi <jessica@example.com>
2025-12-02 21:51:17 +0800

    Fix: Change default MySQL port to 3306

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ MYSQL_HOST = os.getenv('MYSQL_HOST', 'localhost')
 MYSQL_USER = os.getenv('MYSQL_USER', 'portal')
 MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', '123456789')
 MYSQL_DB = os.getenv('MYSQL_DB', 'student_portal')
-MYSQL_PORT = int(os.getenv('MYSQL_PORT', 3308))
+MYSQL_PORT = int(os.getenv('MYSQL_PORT', 3306))
 
 # Uploads configuration
 ALLOWED_IMAGE_EXTENSIONS = {'.png', '.jpg', '.jpeg', '.gif'}
